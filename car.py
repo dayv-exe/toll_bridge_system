e4fabd00064c0f32d729e2bbcdca9c625966c6ab
“david <davzy007@gmail.com>
2024-03-21 16:15:00 +0000

    passes car test

--- car.py
+++ car.py
@@ -1,6 +1,18 @@
+import math
+
 from vehicle import Vehicle
 
 
 class Car(Vehicle):
+
+    def __init__(self, reg_num, weight):
+        self.reg_num = reg_num
+        self.weight = weight
+
     def calculate_fee(self):
-        pass
+        car_fee = 5.00
+        car_max_weight = 1590
+        if self.weight > car_max_weight:
+            car_fee += math.floor(((self.weight - car_max_weight) / 100)) * .10
+
+        return car_fee
