9b619a49a51596d95e30bf249ba8c41b966035fb
“david <davzy007@gmail.com>
2024-03-21 16:08:09 +0000

    passes bike test

--- motorbike.py
+++ motorbike.py
@@ -2,5 +2,10 @@ from vehicle import Vehicle
 
 
 class Motorbike(Vehicle):
+    def __init__(self, reg_num, weight):
+        self.reg_num = reg_num
+        self.weight = weight
+
     def calculate_fee(self):
-        pass
+        motorbike_fee = 3.00
+        return motorbike_fee
