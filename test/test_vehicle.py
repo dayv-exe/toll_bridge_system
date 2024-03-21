a728c111fdc64ff393a2724b0a6caf1d4555126a
Daniel Omogbolahan Adeyemi <6adeyd77@solent.ac.uk>
2024-03-21 14:46:25 +0000

    wrote test for all vehicle

--- test/test_vehicle.py
+++ test/test_vehicle.py
@@ -1,5 +1,24 @@
 import pytest
+from lorry import Lorry
+from motorbike import Motorbike
+from car import Car
+from vehicle import Vehicle
 
 
-def test_dummy():
-    pass
+def test_small_lorry():
+    lorry = Lorry("32", 5000)
+    assert lorry.calculate_fee() == 10
+
+
+def test_motorbike():
+    bike = Motorbike("abc", 400)
+    assert bike.calculate_fee() == 3
+
+
+def test_car():
+    car1 = Car("abc", 1590)
+    assert car1.calculate_fee() == 5
+    car2 = Car("abc", 1690)
+    assert car2.calculate_fee() == 5.10
+
+
