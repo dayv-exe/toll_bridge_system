910b6b4ce4a2321f428a2809090c5edc20793314
“david <davzy007@gmail.com>
2024-03-21 16:19:58 +0000

    passes lorry test

--- lorry.py
+++ lorry.py
@@ -2,5 +2,11 @@ from vehicle import Vehicle
 
 
 class Lorry(Vehicle):
+    def __init__(self, reg_num, weight):
+        self.reg_num = reg_num
+        self.weight = weight
+
     def calculate_fee(self):
-        pass
+        lorry_fee = 10.00 if self.weight  <= 8000 else 15.00
+
+        return lorry_fee
