4de8861b5ae4c4b61b69f7810438e08ebcc9a447
“david <davzy007@gmail.com>
2024-03-21 17:05:09 +0000

    added bridge functionality and passes all tests

--- bridge.py
+++ bridge.py
@@ -5,10 +5,22 @@ class Bridge:
         self.max_weight = 30000
 
     def calc_weight(self):
-        pass
+        total_weight = 0
+        for vehicle in self.vehicles:
+            total_weight += vehicle.weight
+
+        return total_weight
 
     def add_vehicle(self, vehicle):
-        pass
+        if (self.calc_weight() + vehicle.weight) > self.max_weight:
+            return
+        elif len(self.vehicles) + 1 > self.max_vehicles:
+            return
+
+        self.vehicles.append(vehicle)
 
     def remove_vehicle(self, reg_num):
-        pass
+        for vehicle in self.vehicles:
+            if vehicle.reg_num == reg_num:
+                self.vehicles.remove(vehicle)
+                break
--- test/test_bridge.py
+++ test/test_bridge.py
@@ -35,3 +35,32 @@ def test_calc_weight():
     b.add_vehicle(dummy_car2)
 
     assert b.calc_weight() == 10000
+
+
+def test_bridge_weight_limit():
+    dummy_car = Car('ix', 6000)
+    dummy_car1 = Car('v', 1000)
+    dummy_car2 = Car('vi', 3000)
+    dummy_car3 = Car('vii', 5000)
+    dummy_car4 = Car('viii', 13000)
+    dummy_car5 = Car('ix', 7000)
+
+    b = Bridge()
+    b.add_vehicle(dummy_car)
+    b.add_vehicle(dummy_car1)
+    b.add_vehicle(dummy_car2)
+    b.add_vehicle(dummy_car3)
+    b.add_vehicle(dummy_car4)
+    b.add_vehicle(dummy_car5)
+
+    assert len(b.vehicles) == 5
+
+
+def test_bridge_parking_space():
+    b = Bridge()
+
+    for i in range(22):
+        small_car = Car(f"robot-{i}", 1000)
+        b.add_vehicle(small_car)
+
+    assert len(b.vehicles) == 20
